7c6e22af1381ef4546158bd1c569a5babc6ba1c7
Tara Randle <tmrandle@swbell.net>
2021-01-25 19:37:50 -0600

    tried some of the exampls that  i played with in the browser to see how the worked in the page.
    document.write
    console.log
    string variable
    prompt
    embedded js
    linked js

--- js/myScript.js
+++ js/myScript.js
@@ -1,2 +1,15 @@
-var date = new Date();
-document.body.innerHTML = "<h2>Today is " + (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear() + "<h2>";
+var petName = "Cinnamon";
+var yourName;
+var age;
+var classSize;
+
+console.log(petName);  //just testing a variable string
+
+yourName = prompt("What is your Name?");
+
+if (yourName != null) {
+    document.write("Nice to meet you " + yourName + ".");
+}
+
+/*<button type="button" onclick="document.write("Hmmm...maybe the website needs to be refreshed")">Try it</button>
+this is a different way of using the document.write function.  if click everything goes away and what is in the parenthesis is printed on the site-*/
