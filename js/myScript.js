46f5c0669466b71030d75ba0bd8792dbbf29cd40
Tara Randle <tmrandle@swbell.net>
2021-01-27 22:00:52 -0600

    tried to gather mor information using alert, prompt. and worked on the if statment.

--- js/myScript.js
+++ js/myScript.js
@@ -1,15 +1,32 @@
 var petName = "Cinnamon";
 var yourName;
-var age;
-var classSize;
+var x = 10;
+var y = 30;
+sum = x + y;
 
-console.log(petName);  //just testing a variable string
+console.log(petName);  //just testing a variable string using console.log
+
+
 
 yourName = prompt("What is your Name?");
 
-if (yourName != null) {
+if (yourName == null) {
+    alert("You have not entered a name. ");
+}
+else {
+    document.write("It's nice to meet you " + yourName + ".");
+}
+
+
+
+
+
+/*if (yourName != null) {
     document.write("Nice to meet you " + yourName + ".");
 }
+
+
+
 
 /*<button type="button" onclick="document.write("Hmmm...maybe the website needs to be refreshed")">Try it</button>
 this is a different way of using the document.write function.  if click everything goes away and what is in the parenthesis is printed on the site-*/
